f6861da6431ccb72066c7c442ea9b0379764d7c1
Joe <boat232001@yahoo.com>
2023-04-20 21:24:27 +1000

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,17 @@
-#!/bin/python3
-""" Pascal's Triangle """
+#!/usr/bin/python3
+"""
+A pascal's triangle is an arrangement of numbers in a triangular array
+such that the numbers at the end of each row are 1 and the remaining numbers
+are the sum of the nearest two numbers in the above row. This concept is
+used widely in probability, combinatorics, and algebra. Pascal's triangle
+is used to find the likelihood of the outcome of the toss of a coin,
+coefficients of binomial expansions in probability, etc.
+. """
 
 
 def pascal_triangle(n):
-    """ pascal Triangle """
+    """ pascal Triangle
+    """
     if n <= 0:
         return []
     triangle = [[1]]
