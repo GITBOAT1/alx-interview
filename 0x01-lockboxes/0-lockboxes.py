760c69e1d7ad97202811ad9207f657e9f1a17b07
Joe <boat232001@yahoo.com>
2023-04-25 21:24:05 +1000

    lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+def canUnlockAll(boxes):
+    # Set of box numbers that are unlocked
+    unlocked = {0}
+    # List of box numbers that still need to be checked for keys
+    to_check = [0]
+
+    while to_check:
+        box_num = to_check.pop(0)
+        box = boxes[box_num]
+        # Check each key in the current box
+        for key in box:
+            # If the key opens a new box and that box isn't already unlocked
+            if key < len(boxes) and key not in unlocked:
+                unlocked.add(key)
+                to_check.append(key)
+
+    return len(unlocked) == len(boxes)
