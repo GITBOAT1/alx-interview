290789a1b47bf74a8f281ea4ff8eec04f7ec3694
Joe <boat232001@yahoo.com>
2023-05-14 07:57:56 +1000

    keyboard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,3 +20,16 @@ status_codes = {}
 regex = r'^([\d\.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" \
 (\d+) (\d+|-)$'
 pattern = re.compile(regex)
+
+
+try:
+    # read input line by line
+    for i, line in enumerate(sys.stdin, start=1):
+        # check if the line matches the input format
+        match = pattern.match(line.strip())
+        if not match:
+            continue
+
+        # extract the file size and status code from the line
+        file_size = int(match.group(4)) if match.group(4) != '-' else 0
+        status_code = int(match.group(3))
