bcc863ed82a6bcd9d113513ba45d6646438867c6
Joe <boat232001@yahoo.com>
2023-05-14 08:56:43 +1000

    keyboard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -43,3 +43,5 @@ try:
 except KeyboardInterrupt:
     """ print final statistics on keyboard interruption """
     print("Total file size:", total_size)
+    for code in sorted(status_codes):
+        print(f"{code}: {status_codes[code]}")
