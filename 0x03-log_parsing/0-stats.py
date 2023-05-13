fdab34d45894cfadc8cb2cecb62934e24d98a85a
Joe <boat232001@yahoo.com>
2023-05-14 07:46:05 +1000

    keyboard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,17 +5,44 @@ script that reads stdin line by line and computes metrics:
 
 
 import sys
+import re
 
+# regular expression to match the input format
+regex = r'^([\d\.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+|-)$'
+pattern = re.compile(regex)
 
-for line in sys.stdin:
-    try:
-        val = float(line.strip())
-        count += 1
-        total += val
-        if val < min_val:
-            min_val = val
-        if val > max_val:
-            max_val = val
-    except ValueError:
-        # ignore lines that are not valid numbers
-        pass
+# initialize variables
+total_size = 0
+status_codes = {}
+
+try:
+    # read input line by line
+    for i, line in enumerate(sys.stdin, start=1):
+        # check if the line matches the input format
+        match = pattern.match(line.strip())
+        if not match:
+            continue
+
+        # extract the file size and status code from the line
+        file_size = int(match.group(4)) if match.group(4) != '-' else 0
+        status_code = int(match.group(3))
+
+        # update the total file size and the status code count
+        total_size += file_size
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        else:
+            status_codes[status_code] = 1
+
+        # print statistics every 10 lines or on keyboard interruption
+        if i % 10 == 0:
+            print("Total file size:", total_size)
+            for code in sorted(status_codes):
+                print(f"{code}: {status_codes[code]}")
+            print()
+
+except KeyboardInterrupt:
+    # print final statistics on keyboard interruption
+    print("Total file size:", total_size)
+    for code in sorted(status_codes):
+        print(f"{code}: {status_codes[code]}")
