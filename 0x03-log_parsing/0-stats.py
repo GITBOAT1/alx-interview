b45cabb0906ab8dc08cda032c90defe1d33ee93d
Joe <boat232001@yahoo.com>
2023-05-14 07:56:57 +1000

    keyboard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,19 +12,11 @@ import re
 script that reads stdin line by line and computes metrics:
 """
 
-regex = r'^([\d\.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+|-)$'
+
+# initialize variables
+total_size = 0
+status_codes = {}
+
+regex = r'^([\d\.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" \
+(\d+) (\d+|-)$'
 pattern = re.compile(regex)
-
-
-for line in sys.stdin:
-    try:
-        val = float(line.strip())
-        count += 1
-        total += val
-        if val < min_val:
-            min_val = val
-        if val > max_val:
-            max_val = val
-    except ValueError:
-        # ignore lines that are not valid numbers
-        pass
