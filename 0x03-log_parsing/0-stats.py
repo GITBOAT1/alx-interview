ea9f962af670aa9d7f66b0dd91da319ddc7439f4
Joe <boat232001@yahoo.com>
2023-05-14 07:53:29 +1000

    keyboard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,3 +14,15 @@ script that reads stdin line by line and computes metrics:
 
 regex = r'^([\d\.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+|-)$'
 pattern = re.compile(regex)
+
+
+total_size = 0
+status_codes = {}
+
+ode}: {status_codes[code]}")
+            print()
+
+except KeyboardInterrupt:
+    print("Total file size:", total_size)
+    for code in sorted(status_codes):
+        print(f"{code}: {status_codes[code]}")
