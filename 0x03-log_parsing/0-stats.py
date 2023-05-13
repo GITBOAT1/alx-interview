597c35510ebb203d110d93a9a4616fcb492a66d0
Joe <boat232001@yahoo.com>
2023-05-14 08:32:32 +1000

    keyboard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -42,6 +42,9 @@ try:
         # print statistics every 10 lines or on keyboard interruption
         if i % 10 == 0:
             print("Total file size:", total_size)
+            for code in sorted(status_codes):
+                print(f"{code}: {status_codes[code]}")
+            print()
 
 except KeyboardInterrupt:
     """ print final statistics on keyboard interruption """
