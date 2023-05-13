70c1de07e650f152617afd05a9283f001eeae091
Joe <boat232001@yahoo.com>
2023-05-14 08:26:24 +1000

    keyboard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -32,6 +32,12 @@ try:
         # extract the file size and status code from the line
         file_size = int(match.group(4)) if match.group(4) != '-' else 0
         status_code = int(match.group(3))
+        # update the total file size and the status code count
+        total_size += file_size
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        else:
+            status_codes[status_code] = 1
 
 except KeyboardInterrupt:
     """ print final statistics on keyboard interruption """
