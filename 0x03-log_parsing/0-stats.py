fce73fd4a62f2aae3a3a0be40741a867b6d358a7
Joe <boat232001@yahoo.com>
2023-05-14 07:22:25 +1000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,3 +6,16 @@ import sys
 script that reads stdin line by line and computes metrics:
 """
 
+
+for line in sys.stdin:
+    try:
+        val = float(line.strip())
+        count += 1
+        total += val
+        if val < min_val:
+            min_val = val
+        if val > max_val:
+            max_val = val
+    except ValueError:
+        # ignore lines that are not valid numbers
+        pass
