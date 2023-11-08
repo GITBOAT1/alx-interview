db486f2e7c3f6875d59a2e0db5c6ff5c67b20881
Joe <boat232001@yahoo.com>
2023-11-08 21:02:33 +1000

    starwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 const baseUrl = 'https://swapi.dev/api';
 
 // Function to fetch movie details using the provided Movie ID
-function getMovieCharacters(movieId) {
+function getMovieCharacters (movieId) {
   return new Promise((resolve, reject) => {
     // Make a request to fetch movie details
     const movieUrl = `${baseUrl}/films/${movieId}/`;
@@ -21,7 +21,7 @@ function getMovieCharacters(movieId) {
 }
 
 // Function to fetch and print character names
-function printCharacterNames(characterUrls) {
+function printCharacterNames (characterUrls) {
   for (const characterUrl of characterUrls) {
     request(characterUrl, (error, response, body) => {
       if (!error && response.statusCode === 200) {
